511764933871676268ab5878b39f03f768ec5d23
mo-kelvin1 <mintaoseikelvin@gmail.com>
2025-02-17 22:03:15 +0000

    added a switch statement

---
+++ switchstatement.java
@@ -0,0 +1,13 @@
+public class switchstatement{
+    public static void main(String[] args) {
+        int number=3;
+        switch(number){
+            case 1:
+                System.out.println("the number is one");
+            case 2:
+                System.out.println("the number is two");
+            default:
+                System.out.println("the number not one or two") ;   
+        }
+    }
+}
